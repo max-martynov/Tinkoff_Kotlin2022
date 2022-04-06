e165b358a38b1ab09924e3ea19130df5e70c71de
max-martynov <maxon.martynov@gmail.com>
2022-04-06 23:02:05 +0300

    Add tests for paging

--- src/test/kotlin/tinkoff/CitizenControllerTest.kt
+++ src/test/kotlin/tinkoff/CitizenControllerTest.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
@@ -47,31 +48,21 @@ class CitizenControllerTest(
         fun `post normal citizen`() {
             val personalId = 123
             val citizen = Citizen(personalId, fbiClientResponse)
-            mockMvc.perform(
-                post("/citizen/verify")
-                    .param("personalId", "$personalId")
-            )
+            performPost(personalId)
                 .andExpect(status().isOk)
                 .andExpect(content().json(mapper.writeValueAsString(citizen)))
         }
 
         @Test
         fun `post citizen with bad id`() {
-            val personalId = -1
-            mockMvc.perform(
-                post("/citizen/verify")
-                    .param("personalId", "$personalId")
-            )
+            performPost(-1)
                 .andExpect(status().is4xxClientError)
         }
 
         @Test
         fun `post citizen when FBI client doesn't response`() {
             setUpBrokenFBIClient()
-            mockMvc.perform(
-                post("/citizen/verify")
-                    .param("personalId", "123")
-            )
+            performPost(123)
                 .andExpect(status().is5xxServerError)
         }
 
@@ -87,36 +78,92 @@ class CitizenControllerTest(
         fun `get normal existing citizen`() {
             val personalId = 123
             val citizen = Citizen(personalId, fbiClientResponse)
-            mockMvc.perform(
-                post("/citizen/verify")
-                    .param("personalId", "$personalId")
-            )
-            mockMvc.perform(
-                get("/citizen/get/{id}", personalId)
-            )
+            performPost(personalId)
+            performGet(personalId)
                 .andExpect(status().isOk)
                 .andExpect(content().json(mapper.writeValueAsString(citizen)))
         }
 
         @Test
         fun `get non-existent citizen`() {
-            mockMvc.perform(
-                get("/citizen/get/{id}", 123)
-            )
+            performGet(123)
                 .andExpect(status().is4xxClientError)
         }
 
         @Test
         fun `get with an exception from repository`() {
             every { citizenRepository.getCitizen(any()) } throws Exception()
-            mockMvc.perform(
-                get("/citizen/get/{id}", 123)
-            )
+            performGet(123)
                 .andExpect(status().is5xxServerError)
         }
     }
 
+    @Nested
+    inner class GetPageTest {
 
+        private val ids = (1..4).toList()
 
+        private val citizens = ids.map { Citizen(it, fbiClientResponse) }
+
+        @BeforeEach
+        fun addSomeCitizens() {
+            ids.forEach { performPost(it) }
+        }
+
+        @Test
+        fun `get first page`() {
+            performGetPage(1, 2)
+                .andExpect(status().isOk)
+                .andExpect(content().json(mapper.writeValueAsString(citizens.take(2))))
+        }
+
+        @Test
+        fun `get second page`() {
+            performGetPage(2, 2)
+                .andExpect(status().isOk)
+                .andExpect(content().json(mapper.writeValueAsString(citizens.subList(2, 4))))
+        }
+
+        @Test
+        fun `get partial page`() {
+            performGetPage(2, 3)
+                .andExpect(status().isOk)
+                .andExpect(content().json(mapper.writeValueAsString(citizens.subList(3, 4))))
+        }
+
+        @Test
+        fun `get non-existent page`() {
+            performGetPage(5, 1)
+                .andExpect(status().isOk)
+                .andExpect(content().json(mapper.writeValueAsString(listOf<Citizen>())))
+        }
+
+        @Test
+        fun `get page with negative number`() {
+            performGetPage(-1, 4)
+                .andExpect(status().is4xxClientError)
+        }
+
+        @Test
+        fun `get page with zero size`() {
+            performGetPage(2, 0)
+                .andExpect(status().is4xxClientError)
+        }
+    }
+
+    private fun performPost(personalId: Int): ResultActions = mockMvc.perform(
+        post("/citizen/verify")
+            .param("personalId", "$personalId")
+    )
+
+    private fun performGet(personalId: Int): ResultActions = mockMvc.perform(
+        get("/citizen/get/{id}", 123)
+    )
+
+    private fun performGetPage(pageNumber: Int, pageSize: Int): ResultActions = mockMvc.perform(
+        get("/citizen/page")
+            .param("page", "$pageNumber")
+            .param("size", "$pageSize")
+    )
 
 }
