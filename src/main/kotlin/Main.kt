ba9aad92870dc5f8b86769ac8e34cbe02aa4551e
max-martynov <maxon.martynov@gmail.com>
2022-04-14 18:13:32 +0300

    Do something

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
+import java.lang.Thread.sleep
+
+fun someLongOperation(): Long {
+    var s: Long = 0
+    for (i in 2..10_000_000) {
+        var j = 2
+        var prime = true
+        while (j * j <= i) {
+            if (i % j == 0) {
+                prime = false
+                break
+            }
+            j++
+        }
+        if (prime)
+            s += i
+    }
+    return s
+}
+
 fun main() {
-    println("Hello World!")
+    val pool = MyThreadPool(3)
+    repeat(5) {
+        pool.execute {
+            println(it * someLongOperation())
+        }
+    }
+    sleep(1000L)
+    pool.shutdown()
 }
