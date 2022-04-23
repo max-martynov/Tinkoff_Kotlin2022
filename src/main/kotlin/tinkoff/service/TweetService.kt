3cc70c42349a550a50dd23060c8637b7336ae12d
max-martynov <maxon.martynov@gmail.com>
2022-04-23 19:58:53 +0300

    Set up coroutines and web client

--- src/main/kotlin/tinkoff/service/TweetService.kt
+++ src/main/kotlin/tinkoff/service/TweetService.kt
@@ -1,23 +1,31 @@
 package tinkoff.service
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.springframework.cglib.proxy.Dispatcher
+import kotlinx.coroutines.*
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import tinkoff.model.Tweet
 import tinkoff.repository.TweetsRepository
 
 @Service
-class TweetService(private val tweetsRepository: TweetsRepository) {
+class TweetService(
+    private val tweetsRepository: TweetsRepository,
+    private val twitterClient: TwitterClient
+) {
 
-    fun startTrackingTweet(id: Long): ResponseEntity<String> {
-        return ResponseEntity.ok("Successfully received tweet id.")
+    suspend fun startTrackingTweet(id: Long): ResponseEntity<String> {
+        CoroutineScope(Dispatchers.Default).launch {
+            val tweetText = async { twitterClient.getTweetText(id) }
+            val tweetLikesCount = async { twitterClient.getLikesCount(id) }
+            val tweet = Tweet(id, tweetText.await(), tweetLikesCount.await())
+            withContext(Dispatchers.IO) {
+                tweetsRepository.save(tweet)
+            }
+        }
+        return ResponseEntity.ok("Tweet ID successfully received!")
     }
 
     fun getTweetInfo(id: Long): ResponseEntity<Tweet> {
-        return ResponseEntity.ok(Tweet(id, "gg wp", 228))
+        return ResponseEntity.ok(tweetsRepository.get(id))
     }
 
 }
---
+++ src/main/kotlin/tinkoff/service/TwitterClient.kt
@@ -0,0 +1,27 @@
+package tinkoff.service
+
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import org.springframework.web.reactive.function.client.awaitBodyOrNull
+
+
+@Service
+class TwitterClient(
+    private val webClient: WebClient
+) {
+
+    suspend fun getTweetText(id: Long): String =
+       // try {
+            webClient.get().uri("/tweet/text/$id").retrieve().awaitBody()
+      //  } catch (e: Exception) {
+        //    null
+       // }
+
+    suspend fun getLikesCount(id: Long): Int =
+     //   try {
+            webClient.get().uri("/tweet/likes/$id").retrieve().awaitBody()
+   //     } catch (e: Exception) {
+    //        null
+     //   }
+}
