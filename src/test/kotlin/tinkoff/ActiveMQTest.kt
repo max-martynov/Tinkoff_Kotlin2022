35db72befa63b0f43f4630ad6488e59f4eaadb76
max-martynov <maxon.martynov@gmail.com>
2022-05-19 22:56:27 +0300

    Add tests

--- src/test/kotlin/tinkoff/ActiveMQTest.kt
+++ src/test/kotlin/tinkoff/ActiveMQTest.kt
@@ -1,48 +1,36 @@
 package tinkoff
 
-import org.apache.activemq.ActiveMQConnectionFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Primary
-import org.springframework.jms.core.JmsTemplate
 import tinkoff.model.Event
 import tinkoff.model.EventStatus
 import tinkoff.model.EventType
 import tinkoff.repository.EventsRepository
 import java.lang.Thread.sleep
-import javax.jms.Connection
 
-import javax.jms.ConnectionFactory
-import javax.jms.Queue
-import javax.jms.Session
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 
 @SpringBootTest
-class Test(@Autowired private val eventsRepository: EventsRepository) {
-
-//    class ActiveMQConfigTest {
-//
-//        @Bean
-//        @Primary
-//        fun connectionFactory(): ConnectionFactory {
-//            return ActiveMQConnectionFactory("vm://localhost?broker.persistent=false")
-//        }
-//
-//    }
-
+class ActiveMQTest(@Autowired private val eventsRepository: EventsRepository) {
 
     @Test
-    fun `test good scenario`() {
+    fun `test good scenario, ie new event will be sent`() {
         sleep(2000L)
         val event = Event(1, EventType.SMS, "hello!", EventStatus.NEW)
         eventsRepository.addEvent(event)
-        sleep(10000L)
+        sleep(22000L)
         assertEquals(EventStatus.DONE, eventsRepository.getEvent(event.id)?.status)
     }
 
+    @Test
+    fun `test bad scenario, ie errored event won't be sent`() {
+        sleep(2000L)
+        val event = Event(2, EventType.SMS, "hi!", EventStatus.ERROR)
+        eventsRepository.addEvent(event)
+        sleep(22000L)
+        assertEquals(EventStatus.ERROR, eventsRepository.getEvent(event.id)?.status)
+    }
 
 }
